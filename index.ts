694ad2f27bb174603be87b8692dee4940376f863
Kamil Kseń <kamyilksen@gmail.com>
2022-11-21 10:54:30 +0100

    Added new subscribeForOneEventOnly method + tests + updated readme

--- index.ts
+++ index.ts
@@ -1,13 +1,22 @@
 type UnsubscribeFunction = () => void;
+type Options<Events> = {
+  events: Events; 
+  logs?: boolean;
+}
 
 export class PubSub<Events> {
   protected listeners: Record<string, {[id: number]: {
     eventHandler: (data: any) => void;
+    forOneEventOnly: boolean;
   }}> = {};
   protected events!: Events;
   protected logs!: boolean;
   
-  constructor({ events, logs = false }: { events: Events; logs?: boolean }) {
+  /**
+   * Initializes `PubSub` instance
+   * @param Options
+   */
+  constructor({ events, logs = false }: Options<Events>) {
     if (events) this.events = events;
     else
       console.error(
@@ -17,6 +26,11 @@ export class PubSub<Events> {
     if (logs) this.logs = logs;
   }
 
+  /**
+   * Publishes the specified event with data
+   * @param eventName name of the event
+   * @param data 
+   */
   publish<
     EventName extends keyof Events, 
     EventData extends Events[EventName]
@@ -29,6 +43,9 @@ export class PubSub<Events> {
     if (Object.keys(listenersOfThisEvent).length !== 0) {
       for (let listener in listenersOfThisEvent) {
         listenersOfThisEvent[listener].eventHandler(data);
+        if (listenersOfThisEvent[listener].forOneEventOnly) {
+          delete listenersOfThisEvent[listener];
+        }
       }
     } else {
       if (this.logs) {
@@ -37,6 +54,13 @@ export class PubSub<Events> {
     }
   }
 
+  /**
+   * Subscribes to defined event. Every time when this specific event will be published
+   * the eventHandler will be called 
+   * @param eventName The name of event
+   * @param eventHandler callback that will perform on every event publish
+   * @returns Function that allows this subscribe listener to unsubscribe 
+   */
   subscribe<
     EventName extends keyof Events,
     EventData extends Events[EventName]
@@ -52,7 +76,8 @@ export class PubSub<Events> {
     }
 
     this.listeners[eventName as string][newListenerIndex] = {
-      eventHandler: eventHandler
+      eventHandler: eventHandler,
+      forOneEventOnly: false
     };
 
     const unsubscribeHandler = () => {
@@ -60,5 +85,30 @@ export class PubSub<Events> {
     };
     
     return unsubscribeHandler;
+  }
+
+  /**
+   * Subscribes for one event publish only
+   * @param eventName The name of event
+   * @param eventHandler callback that will perform on every event publish
+   */
+  subscribeForOneEventOnly<
+    EventName extends keyof Events,
+    EventData extends Events[EventName]
+  >(eventName: EventName, eventHandler: (data: EventData) => void) {
+    let listenersOfThisEvent = this.listeners[eventName as string];
+    let newListenerIndex: number;
+
+    if (listenersOfThisEvent) {
+      newListenerIndex = Number(Object.keys(listenersOfThisEvent).length + 1);
+    } else {
+      newListenerIndex = 1;
+      this.listeners[eventName as string] = {};
+    }
+
+    this.listeners[eventName as string][newListenerIndex] = {
+      eventHandler: eventHandler,
+      forOneEventOnly: true,
+    };
   }
 }
